e7ebe7cf38a4c5b4468674a5923e97c6be66bf86
SamuelPires1999 <samuel.ferpires@gmail.com>
2022-09-12 22:40:55 -0300

    Server setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('aaa');
+import Koa from 'koa';
+import logger from 'koa-logger';
+import cors from 'kcors';
+import bodyparser from 'koa-bodyparser';
+
+const app = new Koa();
+
+app.use(cors());
+app.use(logger());
+app.use(bodyparser());
+
+app.use(ctx => {
+  ctx.body = 'hello world';
+});
+
+app.listen(3000);
