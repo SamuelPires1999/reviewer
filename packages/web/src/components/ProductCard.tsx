7cf7326766896952f5710caa9ea16b51ec7752a5
SamuelPires1999 <samuel.ferpires@gmail.com>
2022-09-15 17:20:10 -0300

    Adding material ui package for web project || adding first 2 basic ui components

---
+++ packages/web/src/components/ProductCard.tsx
@@ -0,0 +1,54 @@
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Avatar from '@mui/material/Avatar';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+
+interface ExpandMoreProps extends IconButtonProps {
+  expand: boolean;
+}
+
+export default function ProductCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            AN
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="User"
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image="https://image.shutterstock.com/image-vector/default-ui-image-placeholder-wireframes-600w-1037719192.jpg"
+        alt="Placeholder"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          Very cool product, should I buy it?
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton style={{flexGrow: 1}} aria-label="reviews">
+          Reviews 
+          <ArrowForwardIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+}
