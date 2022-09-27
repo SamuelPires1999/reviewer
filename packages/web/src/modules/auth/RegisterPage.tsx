45bfdbdb3a9e40e54a9740b5cf800cde3cf7039b
SamuelPires1999 <samuel.ferpires@gmail.com>
2022-09-27 15:00:58 -0300

    feat(web): Migration register form to react-hook-form

--- packages/web/src/modules/auth/RegisterPage.tsx
+++ packages/web/src/modules/auth/RegisterPage.tsx
@@ -19,134 +19,148 @@ import {
   FormLabel,
   Heading,
   useColorModeValue,
+  Input,
+  FormErrorMessage,
+  useToast,
 } from '@chakra-ui/react';
 import { useStore } from '../../store/useStore';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 export const RegisterPage = () => {
   const { signin } = useAuth();
   const navigate = useNavigate();
-  const [error, setError] = useState({
-    status: false,
-    message: '',
-  });
-
   const setUser = useStore(state => state.setUser);
-
   const [handleUserRegister, isPending] =
     useMutation<AuthRegisterMutationType>(AuthRegisterMutation);
 
-  const formikValue = useFormik({
-    initialValues: { name: '', email: '', password: '' },
-    validateOnMount: true,
-    validationSchema: Yup.object().shape({
-      name: Yup.string().required('User name is required'),
-      email: Yup.string().email().required('Email is required'),
-      password: Yup.string().required('Password is required'),
-    }),
-    onSubmit: (values, actions) => {
-      handleUserRegister({
-        variables: values,
-        onCompleted: ({ RegisterWithEmailMutation }) => {
-          if (RegisterWithEmailMutation?.error) {
-            actions.setSubmitting(false);
-            setError({
-              message: RegisterWithEmailMutation.error,
-              status: true,
-            });
-            return;
-          }
+  const toast = useToast();
 
-          if (RegisterWithEmailMutation?.me) {
-            setUser({
-              _id: RegisterWithEmailMutation.me._id,
-              name: RegisterWithEmailMutation.me.name,
-              email: RegisterWithEmailMutation.me.email,
-            });
-          }
+  type Inputs = {
+    name: string;
+    email: string;
+    password: string;
+  };
 
-          if (RegisterWithEmailMutation?.token) {
-            localStorage.setItem(
-              'CHALLENGE-TOKEN',
-              RegisterWithEmailMutation?.token,
-            );
-          }
-
-          signin(RegisterWithEmailMutation?.token, () => {
-            navigate('/', { replace: true });
-          });
-        },
-      });
-    },
+  const schema = Yup.object({
+    email: Yup.string()
+      .email('The supplied email is invalid')
+      .required('Email is required'),
+    password: Yup.string()
+      .min(6, 'The password must be at least 6 characters long')
+      .required('Password is required'),
+    name: Yup.string().required('A username is required'),
   });
 
-  const { isValid, isSubmitting } = formikValue;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<Inputs>({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit: SubmitHandler<Inputs> = data => {
+    handleUserRegister({
+      variables: data,
+      onCompleted: ({ RegisterWithEmailMutation }) => {
+        if (RegisterWithEmailMutation?.error) {
+          toast({
+            title: 'Error',
+            description: 'Invalid Credentials',
+            status: 'error',
+            duration: 2000,
+          });
+        }
+
+        if (RegisterWithEmailMutation?.token && RegisterWithEmailMutation?.me) {
+          toast({
+            title: 'Success',
+            description: 'Account created',
+            status: 'success',
+            duration: 1000,
+          });
+
+          setUser({
+            _id: RegisterWithEmailMutation.me._id,
+            name: RegisterWithEmailMutation.me.name,
+            email: RegisterWithEmailMutation.me.email,
+          });
+
+          localStorage.setItem(
+            'CHALLENGE-TOKEN',
+            RegisterWithEmailMutation?.token,
+          );
+        }
+
+        signin(RegisterWithEmailMutation?.token, () => {
+          navigate('/', { replace: true });
+        });
+      },
+    });
+  };
 
   return (
     <div>
-      <FormikProvider value={formikValue}>
-        <Form>
-          <Flex
-            minH={'100vh'}
-            align={'center'}
-            justify={'center'}
-            bg={useColorModeValue('gray.50', 'gray.800')}
+      <Flex
+        minH={'100vh'}
+        align={'center'}
+        justify={'center'}
+        bg={useColorModeValue('gray.50', 'gray.800')}
+        as={'form'}
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+          <Stack align={'center'}>
+            <Heading fontSize={'4xl'}>Create your account!</Heading>
+          </Stack>
+          <Box
+            rounded={'lg'}
+            bg={useColorModeValue('white', 'gray.700')}
+            boxShadow={'lg'}
+            p={8}
           >
-            <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-              <Stack align={'center'}>
-                <Heading fontSize={'4xl'}>Create your account!</Heading>
+            <Stack spacing={4}>
+              <FormControl id="email" isInvalid={!!errors.email}>
+                <FormLabel>Email address</FormLabel>
+                <Input {...register('email', { required: true })} />
+
+                {errors.email && (
+                  <FormErrorMessage>{errors.email.message}</FormErrorMessage>
+                )}
+              </FormControl>
+              <FormControl id="name" isInvalid={!!errors.name}>
+                <FormLabel>Name</FormLabel>
+                <Input {...register('name', { required: true })} />
+                {errors.name && (
+                  <FormErrorMessage>{errors.name.message}</FormErrorMessage>
+                )}
+              </FormControl>
+              <FormControl id="password" isInvalid={!!errors.password}>
+                <FormLabel>Password</FormLabel>
+                <Input
+                  type={'password'}
+                  {...register('password', { required: true })}
+                />
+                {errors.password && (
+                  <FormErrorMessage>{errors.password.message}</FormErrorMessage>
+                )}
+              </FormControl>
+              <Stack spacing={10}>
+                <Button
+                  type="submit"
+                  bg={'blue.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'blue.500',
+                  }}
+                >
+                  {isSubmitting ? <Spinner /> : 'Create Account'}
+                </Button>
               </Stack>
-              <Box
-                rounded={'lg'}
-                bg={useColorModeValue('white', 'gray.700')}
-                boxShadow={'lg'}
-                p={8}
-              >
-                <Stack spacing={4}>
-                  <FormControl id="email">
-                    <FormLabel>Email address</FormLabel>
-                    <InputField
-                      name="email"
-                      type="email"
-                      placeholder="email"
-                      shouldValidate
-                    />
-                  </FormControl>
-                  <FormControl id="name">
-                    <FormLabel>Name</FormLabel>
-                    <InputField
-                      name="name"
-                      type="name"
-                      placeholder="name"
-                      shouldValidate
-                    />
-                  </FormControl>
-                  <FormControl id="password">
-                    <FormLabel>Password</FormLabel>
-                    <InputField
-                      name="password"
-                      type="password"
-                      placeholder="Password"
-                      shouldValidate
-                    />
-                  </FormControl>
-                  <Stack spacing={10}>
-                    <Button
-                      type="submit"
-                      bg={'blue.400'}
-                      color={'white'}
-                      _hover={{
-                        bg: 'blue.500',
-                      }}
-                    >
-                      {isSubmitting ? <Spinner /> : 'Create Account'}
-                    </Button>
-                  </Stack>
-                </Stack>
-              </Box>
             </Stack>
-          </Flex>
-        </Form>
-      </FormikProvider>
+          </Box>
+        </Stack>
+      </Flex>
     </div>
   );
 };
