053052cad81bf98f1c139af7683853915e2c8bde
SamuelPires1999 <samuel.ferpires@gmail.com>
2022-09-28 22:49:11 -0300

    feact(server): rewriting some tests

--- packages/server/src/modules/user/__tests__/createUser.test.ts
+++ packages/server/src/modules/user/__tests__/createUser.test.ts
@@ -40,3 +40,35 @@ it('should create a new user', async () => {
   expect(user.me).toBeDefined();
   expect(user.error).toBeNull();
 });
+
+it('should get an error, email not provided', async () => {
+  const mutation = `
+    mutation M($name: String!, $password: String!, $email: String!) {
+      RegisterWithEmailMutation(input: { name: $name, password: $password, email: $email }) {
+        token
+        error
+        me {
+          _id
+          name
+          email
+        }
+      }
+    }
+  `;
+
+  const result = await graphql({
+    schema,
+    source: mutation,
+    rootValue: {},
+    variableValues: {
+      name: 'TestingUser',
+      password: 'TestingPassword',
+    },
+  });
+
+  expect(result.errors).toBeDefined();
+
+  const user = result?.data?.RegisterWithEmailMutation as any;
+
+  expect(user).toBeUndefined();
+});
